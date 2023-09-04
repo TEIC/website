707544aceedce304cb72a8f9860130a6c57bc64b
Hugh A. Cayless <philomousos@gmail.com>
2023-09-04 17:17:49 +0200

    Remove stuff from gh-pages.

--- config/collections.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  posts: function (collectionApi) {
-    return collectionApi.getFilteredByGlob('src/posts/**/*.md');
-  },
-  projects: function (collectionApi) {
-    return collectionApi.getFilteredByGlob('src/projects/**/*.md');
-  }
-}
