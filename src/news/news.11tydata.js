5764fe2257964124e039b0c9684375dd6dae6e86
Peter Stadler <stadlerpeter@yahoo.fr>
2026-09-03 17:23:52 +0200

    honor permalink field

--- src/news/news.11tydata.js
+++ src/news/news.11tydata.js
@@ -2,7 +2,13 @@ export default function() {
   return {
     layout: "news_page.njk",
     permalink: function(data) {
-      return `/news/${this.isoDateToPath(data.date)}/${this.slugify(data.title)}/index.html`;
+      const pattern = /\/news\/\d{4}\/\d{2}\/\d{2}\//;
+      if (pattern.test(data.permalink)) {
+        return data.permalink;
+      }
+      else {
+        return `/news/${this.isoDateToPath(data.date)}/${this.slugify(data.title)}/index.html`;
+      }
     },
     tags: "news",
   };
